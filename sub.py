6c92e505169a192da4f6ed157822ffd3f76f08cc
26Gayatri <gayatriborade26@gmail.com>
2022-10-25 19:39:46 +0530

    more code added in sub.py

--- sub.py
+++ sub.py
@@ -2,5 +2,5 @@ def sub(a,b):
     return a-b
 
 
-print("Subtraction is",sub(14,34))
-print("Subtraction is",sub(167,834))
+print("Subtraction is",sub(897,34))
+print("Subtraction is",sub(89790,8934))
