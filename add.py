14e51025f544c5cd0fe1b8361f519c3df387d1a7
26Gayatri <gayatriborade26@gmail.com>
2022-10-25 19:38:28 +0530

    more code added in sub.py

---
+++ add.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return a+b
+
+
+print("Addition is",add(14,34))
+print("Addition is",add(167,834))
---
+++ sub.py
@@ -0,0 +1,6 @@
+def sub(a,b):
+    return a-b
+
+
+print("Subtraction is",sub(14,34))
+print("Subtraction is",sub(167,834))
